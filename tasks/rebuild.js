bd128ed4060ba142e573a1fc6a0dafb571e03022
lvivier <luke@vivier.ca>
2018-12-20 22:18:17 -0800

    read electron version in rebuild task

--- tasks/rebuild.js
+++ tasks/rebuild.js
@@ -5,11 +5,11 @@ var run = require('./util-run')
 //(cd app && npm rebuild --runtime=electron --target=4.0.0 --disturl=https://atom.io/download/atom-shell --build-from-source); gulp build
 
 gulp.task('rebuild', gulp.series(function () {
-  // TODO read electron version
+  var { version } = require('../node_modules/electron/package.json')
   var cwd = path.join(process.cwd(), 'app')
   console.log(cwd)
   return new Promise(resolve => {
-    run(`npm rebuild --runtime=electron --target=4.0.0 --disturl=https://atom.io/download/atom-shell --build-from-source`, {cwd, shell: true}, function () {
+    run(`npm rebuild --runtime=electron --target=${version} --disturl=https://atom.io/download/atom-shell --build-from-source`, {cwd, shell: true}, function () {
       run(`npm run build`, {shell: true}, function () {
         resolve()
       })
